58cd310963ff5a9a5a6d16c7987e248e0e1288dc
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-24 19:48:03 -0600

    Se importa Bulma al proyecto.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Ecommerce } from './Ecommerce'
+import 'bulma/css/bulma.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
