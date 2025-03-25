814c6e57ead545ea62b3aae67adf57b9cf513107
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-25 11:18:47 -0600

    Se agrega el componente y se le pasan props.

--- src/pages/EcommercePage.jsx
+++ src/pages/EcommercePage.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import '../styles/EcommercePage.css'
+import { BotonInicio } from '../components/BotonInicio'
 
 export const EcommercePage = () => {
   return (
     <div className="columns m-0" id="portadaId">
       <div className="column is-6 column-box bg-image-1">
         <div className="overlay">
-          <button className="button is-light has-background-black-transparent is-rounded has-text-white mt-3" id="botonHombres" data-action="presentar-elementos">Hombres</button>
+          <BotonInicio 
+          titulo={'Hombres'}
+          isMen={true}
+            >
+          </BotonInicio>
         </div>
       </div>
       <div className="column is-6 column-box bg-image-2">
         <div className="overlay">
-          <button className="button is-light has-background-black-transparent is-rounded has-text-white mt-3" id="botonMujeres" data-action="presentar-elementos">Mujeres</button>
+        <BotonInicio 
+          titulo={'Mujeres'}
+          isMen={false}
+          >
+        </BotonInicio>
         </div>
       </div>
     </div>
