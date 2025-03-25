26c95d2079dad80b8e4e7417ddd20a4e0b126014
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-24 21:12:47 -0600

    Mencionas que tu app usara rutas.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Ecommerce } from './Ecommerce'
+import { BrowserRouter } from 'react-router-dom'
 import 'bulma/css/bulma.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Ecommerce></Ecommerce>
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <Ecommerce></Ecommerce>
+    </StrictMode>
+  </BrowserRouter>
 )
