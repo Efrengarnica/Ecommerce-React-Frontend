c2759af4686c63b0563d8ff0b9c382bc89ebdd42
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-24 19:49:25 -0600

    Se importa componente Navbar al componente principal.

--- src/Ecommerce.jsx
+++ src/Ecommerce.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Navbar } from './components/Navbar'
 
 export const Ecommerce = () => {
   return (
-    <div>Ecommerce</div>
+    <>
+      <Navbar></Navbar>
+    </>
   )
 }
