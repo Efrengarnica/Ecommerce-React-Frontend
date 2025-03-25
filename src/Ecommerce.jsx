523f62099f9ec63c734dd98b1b90bd11b1c4e27a
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-24 21:14:07 -0600

    En la app principal agregas los componentes a los que va a responder cada ruta.

--- src/Ecommerce.jsx
+++ src/Ecommerce.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Navbar } from './components/Navbar'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { EcommercePage } from './pages/EcommercePage'
+import { AcercaDePage } from './pages/AcercaDePage'
+import { ContactoPage } from './pages/ContactoPage'
+import { PerfilPage } from './pages/PerfilPage'
 
 export const Ecommerce = () => {
   return (
     <>
       <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<EcommercePage></EcommercePage>}></Route>
+        <Route path='/acercaDe' element={<AcercaDePage></AcercaDePage>}></Route>
+        <Route path='/contacto' element={<ContactoPage></ContactoPage>}></Route>
+        <Route path='/perfil' element={<PerfilPage></PerfilPage>}></Route>
+        <Route path='/*' element={<Navigate to='/'/>}></Route>
+      </Routes>
     </>
   )
 }
