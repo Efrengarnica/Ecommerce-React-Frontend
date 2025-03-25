64a11499b24b94dc6b981785496813ab0f5ece58
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-24 21:13:24 -0600

    Agregas las rutas en el navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
         <nav className="navbar has-background-black" role="navigation" aria-label="main navigation" id="navbar-id">
             <div className="navbar-brand">
-                <a className="navbar-item" href="#">
+                <NavLink to='/' className="navbar-item" href="#">
                     <span className="navbar-item colorLogo">E-commerce</span>
-                </a>
+                </NavLink>
                 <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarMenu">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
@@ -15,9 +16,9 @@ export const Navbar = () => {
             </div>
             <div id="navbarMenu" className="navbar-menu">
                 <div className="navbar-start">
-                    <a className="navbar-item has-text-white">Perfil</a>
-                    <a className="navbar-item has-text-white">Acerca de</a>
-                    <a className="navbar-item has-text-white">Contacto</a>
+                    <NavLink to='/perfil' className="navbar-item has-text-white">Perfil</NavLink>
+                    <NavLink to='/acercaDe' className="navbar-item has-text-white">Acerca de</NavLink>
+                    <NavLink to='/contacto' className="navbar-item has-text-white">Contacto</NavLink>
                 </div>
                 <div className="navbar-end">
                     <div className="navbar-item">
