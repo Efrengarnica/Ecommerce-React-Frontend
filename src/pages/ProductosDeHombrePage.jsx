57da4a7abe153ed4e3200635ec4504054e65d71e
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-25 13:00:48 -0600

    Se importa la store de productos.

--- src/pages/ProductosDeHombrePage.jsx
+++ src/pages/ProductosDeHombrePage.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import useProductosStore from "../store/useProductosStore";
 
 export const ProductosDeHombrePage = () => {
+  const { productos } = useProductosStore();
+  console.log(productos)
   return (
     <div>ProductosDeHombrePage</div>
   )
