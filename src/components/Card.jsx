f47963ea3fa8c3f2f9578af2a4265e7cd0e38b3a
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-25 21:49:49 -0600

    Se crea la card de mi lista de productos con sus estilos.

---
+++ src/components/Card.jsx
@@ -0,0 +1,36 @@
+import "../styles/Card.css";
+import { BotonProducto } from "./BotonProducto";
+
+export const Card = ({ id, title, categoria, image, price, talla }) => {
+    //Falta Realizar esta funcion
+    const clickAgregarACarrito = () => {
+    };
+    return (
+        <div className="column is-3 mb-6 mt-6">
+            <div className="card has-background-gray">
+                <div className="card-image">
+                    <figure className="image">
+                        <img src={`/assets/${ image }`} alt={ title } />
+                    </figure>
+                </div>
+                <div className="card-content mt-0 mb-0">
+                    <div className="media">
+                        <div className="media-content">
+                            <p className="subtitle is-5">{ title }</p>
+                            <p className="subtitle is-6">{ categoria }</p>
+                        </div>
+                    </div>
+                    <div className="content">
+                        <p className="title is-6">MXN ${ price }</p>
+                    </div>
+                </div>
+                <footer className="card-footer mt-0 is-flex is-justify-content-center is-align-items-center">
+                    <BotonProducto
+                    texto={"Agregar a Carrito"}
+                    >   
+                    </BotonProducto>
+                </footer>
+            </div>
+        </div>
+    )
+}
