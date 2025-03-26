6e3b51c72fdcd571dfac21b9674fcb1bd09678b6
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-26 15:28:06 -0600

    Se eliminan comentarios.

--- src/components/BuscadorProductos.jsx
+++ src/components/BuscadorProductos.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from 'react'
 
-//Funcion para modifircar el contesxt
-//Se rreendizara Productos hombre 
-
 export const BuscadorProductos = ({ stagePage, modificarStatePage }) => {
-
     return (
         <div className="control">
             <input 
