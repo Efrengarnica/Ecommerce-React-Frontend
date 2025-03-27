ff0a0a1b9343e196539842aebe6b3a176a237858
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-26 21:09:23 -0600

    Se corrige estilos de escritura.

--- src/components/BotonProducto.jsx
+++ src/components/BotonProducto.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import '../styles/BotonProducto.css'
 
-export const BotonProducto = ( {texto} ) => {
+export const BotonProducto = ({ texto }) => {
   return (
-    <button type="button" className="agregarACarrito button is-light has-background-black has-text-white mt-3">{texto}</button>
+    <button 
+    type="button" 
+    className="agregarACarrito button is-light has-background-black has-text-white mt-3"
+    >
+    {texto}
+    </button>
   )
 }
