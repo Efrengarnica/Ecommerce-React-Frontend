4efeebe855c86b05109bf809a9a8cca1cbc8c23f
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-28 23:33:18 -0600

    Hacemos que la Card acepte el id, después se lo pasamos al botón de la Card para que este pueda usar una función que trajo del store de Cart y así poder agregar el producto al carrito.

--- src/components/BotonProducto.jsx
+++ src/components/BotonProducto.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import '../styles/BotonProducto.css'
+import useCartStore from '../store/useCartStore'
 
-export const BotonProducto = ({ texto }) => {
+export const BotonProducto = ({ texto, id }) => {
+  const modificarCarrito = useCartStore((state) => state.modificarCarrito);
   return (
     <button 
     type="button" 
     className="agregarACarrito button is-light has-background-black has-text-white mt-3"
+    onClick={() => modificarCarrito(id)}
     >
     {texto}
     </button>
