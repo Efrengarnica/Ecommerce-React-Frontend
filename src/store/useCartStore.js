510ad4c282c78bd258b44bb10e4db93bc5bd037e
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-04-09 15:00:57 -0600

    Se implementan los estados necesarios para poder mostrar mensajes de error en alguna petición, al usuario, del carrito o de los productos.

--- src/store/useCartStore.js
+++ src/store/useCartStore.js
@@ -13,14 +13,19 @@ const useCartStore = create((set, get) => ({
 
     //Estados para mostrar el modal de compra exitosa.
     isModalPurchaseOpen: false,
+    isloadingCompraUsuario: false,
+    errorAlComprarProductos: false,
     openModalPurchase: async(userCartId, userId) => {
       if (get().carritoComprasUsuario?.items?.length === 0)  return
-      set({ 
-        isCartOpen:false,
-        isModalPurchaseOpen:true,
-       })
-      await clearCartItems(userCartId)
-      await get().fetchCartUsuario(userId)
+      set({ isModalPurchaseOpen:true, isCartOpen:false, isloadingCompraUsuario: true, errorAlComprarProductos: false })
+      try {
+        await clearCartItems(userCartId)
+        await get().fetchCartUsuario(userId)
+      } catch(error){
+        set({ errorAlComprarProductos: true })
+      } finally {
+        set({ isloadingCompraUsuario: false })
+      }
     },
     closeModalPurchase: () => set({ isModalPurchaseOpen: false }),
     
@@ -35,59 +40,76 @@ const useCartStore = create((set, get) => ({
 
     //Estado del carrito del usuario.
     carritoComprasUsuario: null,
-    errorCarrito: null,
+    errorCargarCarrito: false,
+    isLoadingCarrito: false,
     fetchCartUsuario: async (userId) => {
+      set({ isLoadingCarrito: true, errorCargarCarrito: false })
       try {
         const carrito = await getCarritoUser(userId)
         set({carritoComprasUsuario: carrito})
       } catch (error) {
-        console.error("Error al cargar el carrito:", error)
-        set({errorHombre: "Error al cargar el carrito"})
+        set({ errorCargarCarrito: true })
+      } finally {
+        set({ isLoadingCarrito: false })
       }
     },
+
     // Función para agregar o actualizar un producto en el carrito.
+    errorModificarCarrito : false,
+    cerrarErrorModificarCarrito: () => set({ errorModificarCarrito: false }),
+    isLoadingModificarCarrito: false,
     updateCartItem: async (productId, operacion, userCartId, userId, quantityDelta = 1) => {
       let data = null
       let idItemAModificar=null
       const cart = get().carritoComprasUsuario;
       if (!cart) return; // Si no hay carrito cargado, no hacer nada
-      let updatedItems = [...cart.items];
-        // Buscar si el producto ya está en el carrito
-      let itemIndex = updatedItems.findIndex(item => Number(item.product_id) === Number(productId));
-      if(operacion==="suma"){
-        if (itemIndex > -1) {
-          data = {"quantity": updatedItems[itemIndex].quantity + quantityDelta}
-          idItemAModificar = updatedItems[itemIndex].id
-          await editCartItem(data, idItemAModificar)
-        } else {
-          data = {
-            "cart_id": userCartId,
-            "product_id": productId,
-            "quantity": quantityDelta,
-          }
-          await postCarritoItem(data)
-        }
-      }else if(operacion==="resta"){
-        if (itemIndex > -1) {
-          if(updatedItems[itemIndex].quantity - quantityDelta > 0){
-            data = {"quantity": updatedItems[itemIndex].quantity - quantityDelta}
+      set({ isLoadingModificarCarrito: true, errorModificarCarrito: false })
+      try{
+        let updatedItems = [...cart.items];
+          // Buscar si el producto ya está en el carrito
+        let itemIndex = updatedItems.findIndex(item => Number(item.product_id) === Number(productId));
+        if(operacion==="suma"){
+          if (itemIndex > -1) {
+            data = {"quantity": updatedItems[itemIndex].quantity + quantityDelta}
             idItemAModificar = updatedItems[itemIndex].id
             await editCartItem(data, idItemAModificar)
           } else {
-            await deleteCartItem(updatedItems[itemIndex].id)
+            data = {
+              "cart_id": userCartId,
+              "product_id": productId,
+              "quantity": quantityDelta,
+            }
+            await postCarritoItem(data)
+          }
+        }else if(operacion==="resta"){
+          if (itemIndex > -1) {
+            if(updatedItems[itemIndex].quantity - quantityDelta > 0){
+              data = {"quantity": updatedItems[itemIndex].quantity - quantityDelta}
+              idItemAModificar = updatedItems[itemIndex].id
+              await editCartItem(data, idItemAModificar)
+            } else {
+              await deleteCartItem(updatedItems[itemIndex].id)
+            }
+          } else {
+            return
           }
         } else {
-          return
+          if (itemIndex > -1) {
+            await deleteCartItem(updatedItems[itemIndex].id)}
+          else{
+            return
+          }
         }
-      } else {
-        if (itemIndex > -1) {
-          await deleteCartItem(updatedItems[itemIndex].id)}
-        else{
-          return
-        }
-      }
-      await get().fetchCartUsuario(userId)
+    } catch(error){
+      // Error al modificar el carrito (post/edit/delete)
+      console.error("Error al modificar ítem del carrito:", error);
+      set({ errorModificarCarrito: true });
+    } finally {
+      set({ isLoadingModificarCarrito: false });
+    }
+    await get().fetchCartUsuario(userId)
     },
+
     // Función que calcula el total de productos en el carrito y que se muestra en el navbar.
     calcularTotalProductos: () => {
       if(get().carritoComprasUsuario !== null){
@@ -96,6 +118,7 @@ const useCartStore = create((set, get) => ({
         }, 0);
       }
     },
+    
     // Función que calcula el Total de dinero que se va a pagar y que se muestra en el carrito.
     calcularTotal: () => {
       if(get().carritoComprasUsuario !== null){
