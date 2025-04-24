19612badcde7013e5483e6735292d0407740bb4e
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-04-23 19:29:25 -0600

    Se modificó lo necesario en editUser para que se lancen los errores correspondientes para saber si un email ya esta en uso o errores del servidor. También se implementó la función necesaria para poder cambiar la contraseña del usuario.

--- api/requestsUser.js
+++ api/requestsUser.js
@@ -38,6 +38,35 @@ async function saveUser(user) {
         },
         body: JSON.stringify(user),
     })
+     //Ya existe ese email, tiene que elegir otro.
+    if (response.status === 400) {
+        throw new Error("Ese email ya está en uso por otro usuario.");  
+    }
+
+    if (!response.ok) {
+        throw new Error("Error save user");
+    }
+
+    const userSaved = await response.json()
+    return userSaved
+}
+
+//Me ayuda a registrar un user y crear un carrito al mismo tiempo, a pesar de eso se sigue devolviendo un user.
+//Por que tengo este método y no ocupé los 2 por separado, la razon es que puede que si ocupo los dos por separado ocurra un error al crear el cart
+//y si ya se habia creado el user entonces tendria un user sin cart.
+async function saveUserAndCreateCart(user) {
+    const response = await fetch("http://localhost:8001/users/andCart", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    })
+     //Ya existe ese email, tiene que elegir otro.
+    if (response.status === 400) {
+        throw new Error("Ese email ya está en uso por otro usuario.");  
+    }
+    //Cualquier otro error.
     if (!response.ok) {
         throw new Error("Error save user");
     }
@@ -70,6 +99,11 @@ async function editUser(idUser, data) {
         },
         body: JSON.stringify(data),
     })
+    //Ya existe ese email, tiene que elegir otro.
+    if (response.status === 400) {
+        throw new Error("Ese email ya está en uso por otro usuario.");  
+    }
+    //Cualquier otro error.
     if (!response.ok) {
         throw new Error("Error edit user");
     }
@@ -77,4 +111,25 @@ async function editUser(idUser, data) {
     return user
 }
 
-export { getUser, saveUser, editAllUser, editUser}
+//Me ayuda a modificar la contraseña del usuario siemore y cuando nos dé la contraseña actual.
+async function editUserPassword(idUser, dataPassword) {
+    const response = await fetch(`http://localhost:8001/users/${idUser}/changePassword`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataPassword),
+    })
+    //Error que muestra cuando las credenciales son inválidas
+    if (response.status === 401) {
+        throw new Error("La contraseña que proporcionaste es incorrecta.");  
+    }
+    //Cualquier otro error.
+    if (!response.ok) {
+        throw new Error("Error edit user");
+    }
+    const user = await response.json()
+    return user
+}
+
+export { getUser, saveUser, editAllUser, editUser, saveUserAndCreateCart, editUserPassword}
