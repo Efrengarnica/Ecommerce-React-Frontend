e21001ff6ba33893d2cc5947b3a7048862fa7ee2
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-04-23 19:34:08 -0600

    Se implementa la forma de modificar los datos del usuario, menos la contraseña, con sus respectivas alertas si el usuario se llega a equivocar. También se implementa la manera de crear un usuario y un carrito al mismo tiempo, con la finalidad de que nos ayude en el form de registro. Ya por último se implementa la forma de poder cambiar sólo la contraseña del usuario con base en su contraseña actual.

--- src/store/useUserStore.js
+++ src/store/useUserStore.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { getUser, saveUser, editAllUser, editUser } from '../../api/requestsUser'
+import { getUser, saveUser, editAllUser, editUser, saveUserAndCreateCart, editUserPassword } from '../../api/requestsUser'
 
 const useUserStore = create((set, get) => ({
 
@@ -44,14 +44,75 @@ const useUserStore = create((set, get) => ({
       })
     },
 
-    //Modifica los datos del usuario activo.
+    // Modifica los datos del usuario activo, menos la contraseña.
+    isLoadingModificarDatosUsuario: false,
+    errorModificarDatosUsuarioEmailEnUso: false,
+    errorModificarDatosUsuarioFallaServidor: false,
+    modalModificarDatosUsuario: false,
+    exitoAlModificarDatosUsuario: false,
+    cerrarModalModificarDatosUsuario: () => {set({modalModificarDatosUsuario:false})},
     modificarDatosUsuario: async (idUser, data) => {
+      set({isLoadingModificarDatosUsuario: true, errorModificarDatosUsuarioEmailEnUso: false, errorModificarDatosUsuarioFallaServidor: false, 
+        modalModificarDatosUsuario:true, exitoAlModificarDatosUsuario: false})
       try {
         const user = await editUser(idUser, data)
-        set({dataUser:user})
-
+        set({dataUser:user, exitoAlModificarDatosUsuario: true})
       } catch(error){
-        console.error("Error al modificar al usuario:", error)
+        if (error.message === "Ese email ya está en uso por otro usuario.") {
+          set({ errorModificarDatosUsuarioEmailEnUso: true});
+        } else {
+          set({ errorModificarDatosUsuarioFallaServidor: true });
+        }
+      } finally {
+        set({isLoadingModificarDatosUsuario: false})
+      }
+    },
+
+    // Me ayuda a crear un usuario y su carrito a la vez.
+    isLoadingCrearUsuario: false,
+    errorCrearUsuarioEmailEnUso: false,
+    errorCrearUsuarioFallaServidor:false,
+    modalCrearUsuario: false,
+    exitoAlCrearUsuario:false,
+    cerrarModalFracasoAlCrearUsuario: () => {set({modalCrearUsuario:false})},
+    crearUsuario: async(userData) => {
+      set({isLoadingCrearUsuario:true, errorCrearUsuarioEmailEnUso: false, modalCrearUsuario:true, 
+        errorCrearUsuarioFallaServidor:false, exitoAlCrearUsuario:false})
+      try {
+        await saveUserAndCreateCart(userData)
+        set({exitoAlCrearUsuario: true})
+      } catch(error){
+        if (error.message === "Ese email ya está en uso por otro usuario.") {
+          set({ errorCrearUsuarioEmailEnUso: true});
+        } else {
+          set({ errorCrearUsuarioFallaServidor: true });
+        }
+      } finally {
+        set({isLoadingCrearUsuario: false})
+      }
+    },
+
+    //Me ayuda a modificar la contraseña del usuario con base en su contraseña actual.
+    isLoadingModificarContraseñaUsuario: false,
+    errorModificarContraseñaUsuarioContraseñaIncorrecta: false,
+    errorModificarContraseñaUsuarioFallaServidor: false,
+    modalModificarContraseñaUsuario: false,
+    exitoAlModificarContraseñaUsuario: false,
+    cerrarModalModificarContraseñaUsuario: () => {set({modalModificarContraseñaUsuario:false})},
+    modificarContraseñaUsuario: async (idUser, dataPassword) => {
+      set({isLoadingModificarContraseñaUsuario: true, errorModificarContraseñaUsuarioContraseñaIncorrecta: false, errorModificarContraseñaUsuarioFallaServidor: false, 
+        modalModificarContraseñaUsuario:true, exitoAlModificarContraseñaUsuario: false})
+      try {
+        const user = await editUserPassword(idUser, dataPassword)
+        set({dataUser:user, exitoAlModificarContraseñaUsuario: true})
+      } catch(error){
+        if (error.message === "La contraseña que proporcionaste es incorrecta.") {
+          set({ errorModificarContraseñaUsuarioContraseñaIncorrecta: true});
+        } else {
+          set({ errorModificarContraseñaUsuarioFallaServidor: true });
+        }
+      } finally {
+        set({isLoadingModificarContraseñaUsuario: false})
       }
     }
 
