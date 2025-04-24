7fa465950ddb5d1bd9b1669f2429bf84ab6df85c
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-04-23 19:41:42 -0600

    Se evita que cuando se guarden las cosas del formulario de perfil page no se recargue la página. Se implementa la manera de poder regresar los valores a como estaban si el cliente le da cancelar. También se implementa la forma de cambiar la contraseña del usuario.

--- src/pages/PerfilPage.jsx
+++ src/pages/PerfilPage.jsx
@@ -3,6 +3,9 @@ import { Form } from '../components/Form'
 import useUserStore from '../store/useUserStore'
 import { data } from 'react-router-dom'
 import '../styles/PerfilPage.css'
+import { ModalCambiosPerfil } from '../components/ModalCambiosPerfil'
+import { ModalCambioContraseña } from '../components/ModalCambioContraseña'
+import { ModalAvisosCambioContraseña } from '../components/ModalAvisosCambioContraseña'
 
 export const PerfilPage = () => {
   
@@ -16,8 +19,9 @@ export const PerfilPage = () => {
     setData({ ...data, ...updatedData });
   }
 
-  async function guardarData() {
-    if (data.name === "" || data.email === "" || data.password === "") {
+  async function guardarData(e) {
+    e.preventDefault(); 
+    if (data.name === "" || data.email === "") {
       alert("Por favor llena todos los campos");
       return;
     }
@@ -25,18 +29,75 @@ export const PerfilPage = () => {
       ...data
     };
     await modificarDatosUsuario( dataUser.id, newUser);
-    window.alert("Se guardaron los datos correctamente")
   }
+
+  //Funcion que me ayuda a regresar los valores a como estaban antes.
+  //Se vuelve a poner los valores iniciales, pero una copia MEJOR.
+  const regresarValoresAComoEstaban = () => {
+    setData({...dataUser})
+  }
+
+
+  //Valores que me ayudan a abrir el modal de cambio de contraseña.
+  const [estaAbiertoModalCambioContraseña, setEstaAbiertoModalCambioContraseña] = useState(false)
+  const abrirModalCambioContraseña = () => {
+    setEstaAbiertoModalCambioContraseña(true)
+  }
+  const cerrarModalCambioContraseña = () => {
+    setEstaAbiertoModalCambioContraseña(false)
+  }
+
+  // Esto es para modificar solo la contraseña
+  const [dataContraseña, setDataContraseña] = useState({})
+  const modificarContraseñaUsuario = useUserStore(state => state.modificarContraseñaUsuario)
+
+  async function updateDataContraseña(updatedDataContraseña) {
+    setDataContraseña({ ...dataContraseña, ...updatedDataContraseña });
+  }
+  
+  async function guardarContraseña(e) {
+    e.preventDefault(); 
+    if(Object.keys(dataContraseña).length < 3){
+      alert("Por favor llena todos los campos");
+      return
+    }
+    if (dataContraseña.passwordActual === "" || dataContraseña.passwordNuevo === "" || dataContraseña.passwordRepeticion === "") {
+      alert("Por favor llena los campos");
+      return;
+    }
+    if (dataContraseña.passwordNuevo !== dataContraseña.passwordRepeticion){
+      alert("La contraseña nueva que quieres actualizar no coincide con su repetición.Intenta de nuevo.")
+      return
+    }
+    const newDataPassword = {
+      "passwordActual": dataContraseña.passwordActual,
+      "passwordNuevo": dataContraseña.passwordNuevo
+    };
+    await modificarContraseñaUsuario( dataUser.id, newDataPassword);
+    
+  }
+
 
   return (
     isActive ? (
       <section className="section mt-6" >
         <div className="container">
+          <ModalCambiosPerfil/>
+          
+          <ModalCambioContraseña
+            estadoDelModal= {estaAbiertoModalCambioContraseña}
+            cerrarModal= {cerrarModalCambioContraseña}
+            modificarContraseña= {updateDataContraseña}
+            guardarCambios= {guardarContraseña}
+          />
+          <ModalAvisosCambioContraseña/>
           <h1 className="title">Datos del usuario</h1>
           <Form
-            dataUsuario={dataUser}
+            dataUsuario={data}
             modificarData={updateData}
             guardarCambios={guardarData}
+            resetarValores={regresarValoresAComoEstaban}
+            abrirModal= {abrirModalCambioContraseña}
           >
           </Form>
         </div>
